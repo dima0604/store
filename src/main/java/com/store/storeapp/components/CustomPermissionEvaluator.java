21a195182f50aac5d664152fd9664365a479242b
dima <dima11110604@gmail.com>
2023-11-14 13:14:47 +0200

    init

---
+++ src/main/java/com/store/storeapp/components/CustomPermissionEvaluator.java
@@ -0,0 +1,22 @@
+package com.store.storeapp.components;
+
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class CustomPermissionEvaluator implements PermissionEvaluator {
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        return true;
+    }
+}
